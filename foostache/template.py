86df7ca56db2571e65aa6c662cc132e41bdcb204
Lynn Gabbay <gabbay@gmail.com>
2015-08-04 15:22:45 -0700

    moved printer to method of Template

--- foostache/template.py
+++ foostache/template.py
@@ -4,10 +4,24 @@ import antlr4
 
 import parser.FoostacheLexer as FoostacheLexer
 import parser.FoostacheParser as FoostacheParser
+import parser.FoostacheParserListener as FoostacheParserListener
 from visitor import Visitor
 
 
 class Template(object):
+    class Printer(FoostacheParserListener.FoostacheParserListener):
+        def __init__(self):
+            super(Template.Printer, self).__init__()
+            self.indent = 0
+
+        def enterEveryRule(self, ctx):
+            print ' ' * self.indent + "{}: {}".format(type(ctx).__name__[:-7], ctx.getText())
+            self.indent += 1
+
+        def exitEveryRule(self, ctx):
+            self.indent -= 1
+
+
     def __init__(self, data):
         if not isinstance(data, unicode):
             raise ValueError('data is not unicode')
@@ -17,6 +31,11 @@ class Template(object):
         token_stream = antlr4.CommonTokenStream(lexer)
         parser = FoostacheParser.FoostacheParser(token_stream)
         self._tree = parser.template()
+
+    def dump(self):
+        printer = Template.Printer()
+        walker = antlr4.ParseTreeWalker()
+        walker.walk(printer, self._tree)
 
     def render(self, context):
         v = Visitor(context)
