45ffa162d1144b262a06517394a77343f4940744
Lynn Gabbay <gabbay@gmail.com>
2019-01-12 09:55:41 -0800

    fixed python_requires

--- setup.py
+++ setup.py
@@ -75,5 +75,5 @@ setup(
     test_suite="tests",
     keywords='foostache mustache',
     install_requires=INSTALL_REQUIRES,
-    python_requires='~=2.7, ~=3.4',
+    python_requires='>=2.7, !=3.0.*, !=3.1.*, !=3.2.*, !=3.3.*, <4',
 )
