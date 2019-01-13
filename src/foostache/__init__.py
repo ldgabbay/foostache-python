4661fd7ffc84d96281e619cb024ce2b35ccb51f8
Lynn Gabbay <gabbay@gmail.com>
2019-01-13 13:25:35 -0800

    bump

--- src/foostache/__init__.py
+++ src/foostache/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 from builtins import (ascii, bytes, chr, dict, filter, hex, input, int, map, next, oct, open, pow, range, round, str, super, zip)
 
-__version__ = "1.2.0"
+__version__ = "1.2.1.dev0"
 
 import sys
 
