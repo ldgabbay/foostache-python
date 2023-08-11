f8668cd6369ac3db4b2ba0a8e4507e296fa9549c
Lynn Gabbay <gabbay@gmail.com>
2023-08-11 12:38:35 +0700

    bumped to 1.3.0

--- src/foostache/__init__.py
+++ src/foostache/__init__.py
@@ -1,7 +1,7 @@
 from __future__ import (absolute_import, division, print_function, unicode_literals)
 from builtins import (ascii, bytes, chr, dict, filter, hex, input, int, list, map, next, object, oct, open, pow, range, round, str, super, zip)
 
-__version__ = "1.2.1"
+__version__ = "1.3.0"
 
 import sys
 
